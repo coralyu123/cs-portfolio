58b44bfba95f31d215399cf204623661b4c51a99
coralyu123 <cyu.sh@mysummitps.org>
2016-06-02 21:20:37 +0000

    upgrade

--- calc.js
+++ calc.js
@@ -12,15 +12,24 @@ function doMath() {
         answer = parseInt(input1.value) + parseInt(input2.value);
         display.innerHTML = answer;
     }
-    if  (dropdown.value == "-"){
-        answer = parseInt(input1.value) + parseInt(input2.value);
+    if (dropdown.value == "-"){
+        answer = parseInt(input1.value) - parseInt(input2.value);
         display.innerHTML = answer;
     }
-    if  (dropdown.value == "*"){
-        answer = parseInt(input1.value) + parseInt(input2.value);
+    if (dropdown.value == "x"){
+        answer = parseInt(input1.value) * parseInt(input2.value);
         display.innerHTML = answer;
     }
     if (dropdown.value == "/"){
-        answer = parseInt(input1.value) + parseInt(input2.value);
+        answer = parseInt(input1.value) / parseInt(input2.value);
         display.innerHTML = answer;
     }
+    if (dropdown.value == "^"){
+        answer = Math.pow(input1.value, input2.value);
+        display.innerHTML = answer;
+    }
+    if (dropdown.value == "nroot"){
+        answer = Math.pow(input1.value, 1 / input2.value);
+        display.innerHTML = answer;
+    }
+}
