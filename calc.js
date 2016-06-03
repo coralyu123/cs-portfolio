5222c927ee15862e1eb7b7e67add3421787ca78a
coralyu123 <cyu.sh@mysummitps.org>
2016-06-03 16:02:54 +0000

    upgrade

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//
 var button = document.getElementById("submitButton");
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
@@ -5,9 +6,12 @@ var answer;
 var display = document.getElementById('display');
 var dropdown = document.getElementById("dropdown");
 
+//
 button.addEventListener("click", doMath);
 
+//
 function doMath() {
+    //
     if  (dropdown.value == "+"){
         answer = parseInt(input1.value) + parseInt(input2.value);
         display.innerHTML = answer;
